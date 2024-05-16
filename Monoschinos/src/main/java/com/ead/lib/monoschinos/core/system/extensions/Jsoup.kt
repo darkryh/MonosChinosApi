a17083e06e9b68ccd4247dc839d663b60e51c26c
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-16 09:20:51 -0500

    extension class for Jsoup

--- Monoschinos/src/main/java/com/ead/lib/monoschinos/core/system/extensions/Jsoup.kt
+++ Monoschinos/src/main/java/com/ead/lib/monoschinos/core/system/extensions/Jsoup.kt
@@ -1,7 +1,10 @@
 package com.ead.lib.monoschinos.core.system.extensions
 
+import com.ead.lib.monoschinos.core.Properties
 import kotlinx.coroutines.suspendCancellableCoroutine
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import kotlin.coroutines.resume
 
 suspend fun Document.suspend() : Document? {
@@ -11,4 +14,22 @@ suspend fun Document.suspend() : Document? {
             in 200..299 -> continuation.resume(this)
         }
     }
+}
+
+fun Elements.getSrcAttr(): String {
+    val result = attr("src")
+
+    if (result == Properties.CAP_BLANK_MC2 || result == Properties.CAP_BLANK_ANIME_MC2  || result.isEmpty()) {
+        return "data-src"
+    }
+    return "src"
+}
+
+fun Element.getSrcAttr(selectorQuery : String) : String {
+    val result = select(selectorQuery).attr("src")
+
+    if (result == Properties.CAP_BLANK_MC2 || result == Properties.CAP_BLANK_ANIME_MC2  || result.isEmpty()) {
+        return "data-src"
+    }
+    return "src"
 }

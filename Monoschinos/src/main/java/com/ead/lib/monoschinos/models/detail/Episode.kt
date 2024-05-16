ad985a695ea40abeba06addeb9cfce2a51a7e405
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-16 15:09:03 -0500

    removed title

--- Monoschinos/src/main/java/com/ead/lib/monoschinos/models/detail/Episode.kt
+++ Monoschinos/src/main/java/com/ead/lib/monoschinos/models/detail/Episode.kt
@@ -3,7 +3,6 @@ package com.ead.lib.monoschinos.models.detail
 import com.ead.lib.monoschinos.core.Properties
 
 data class Episode(
-    val title : String,
     val number : Int,
     val image : String,
     val url : String

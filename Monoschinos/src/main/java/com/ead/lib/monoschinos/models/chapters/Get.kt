988a78b5a83e4e846f909ee2a88a785ad4ed40b3
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-16 15:09:26 -0500

    Get class for episodes

---
+++ Monoschinos/src/main/java/com/ead/lib/monoschinos/models/chapters/Get.kt
@@ -0,0 +1,15 @@
+package com.ead.lib.monoschinos.models.chapters
+
+import android.content.Context
+import com.ead.lib.monoschinos.core.Network
+import com.ead.lib.monoschinos.models.detail.Episode
+
+class Get(
+    private val context: Context,
+    private val seo : String
+) {
+
+    suspend fun get() : List<Episode> {
+        return Network.episodes(seo,context)
+    }
+}

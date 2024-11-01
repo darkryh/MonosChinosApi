72fc4073027002d870166a0f94e34c9bb2b408b9
darkryh <alex_torres-xc@hotmail.com>
2024-10-31 23:49:40 -0500

    move data class

--- Monoschinos/src/main/java/com/ead/lib/monoschinos/models/Home.kt
+++
@@ -1,9 +0,0 @@
-package com.ead.lib.monoschinos.models
-
-import com.ead.lib.monoschinos.models.home.HomeChapter
-import com.ead.lib.monoschinos.models.home.HomeAnime
-
-data class Home(
-    val lastChapters : List<HomeChapter>,
-    val recentSeries : List<HomeAnime>
-)
---
+++ Monoschinos/src/main/java/com/ead/lib/monoschinos/models/home/Home.kt
@@ -0,0 +1,6 @@
+package com.ead.lib.monoschinos.models.home
+
+data class Home(
+    val lastChapters : List<HomeChapter>,
+    val recentSeries : List<HomeAnime>
+)

e56b6887d816fd688a38ebb46dbb26b6c9220b30
darkryh <alex_torres-xc@hotmail.com>
2024-11-03 12:00:30 -0500

    upgrade lower apis to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ead.app.monoschinos"
-        minSdk = 26
+        minSdk = 21
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"

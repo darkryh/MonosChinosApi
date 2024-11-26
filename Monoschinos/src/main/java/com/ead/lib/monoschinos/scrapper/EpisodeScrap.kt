2bb9172fc6c1266454b6add4fc3e7fa86e6ee3b9
darkryh <alex_torres-xc@hotmail.com>
2024-11-25 19:24:34 -0500

    episode query

---
+++ Monoschinos/src/main/java/com/ead/lib/monoschinos/scrapper/EpisodeScrap.kt
@@ -0,0 +1,81 @@
+package com.ead.lib.monoschinos.scrapper
+
+import com.ead.lib.monoschinos.core.Properties.CHAPTER_EPISODES
+import com.ead.lib.monoschinos.core.Properties.CHAPTER_EPISODES_PAGINATE
+import com.ead.lib.monoschinos.core.Properties.CHAPTER_EPISODES_PER_PAGE
+import com.ead.lib.monoschinos.core.Properties.CSRF_TOKEN
+import com.ead.lib.monoschinos.core.Properties.CSS_SELECTOR_INTERNAL_API
+import com.ead.lib.monoschinos.core.Properties.PAYLOAD_PAGE
+import com.ead.lib.monoschinos.core.Properties.PAYLOAD_TOKEN
+import com.ead.lib.monoschinos.core.connection.RestClient
+import com.ead.lib.monoschinos.core.system.extensions.toEpisodeList
+import com.ead.lib.monoschinos.models.detail.Episode
+import okhttp3.FormBody
+import okhttp3.Headers
+import org.json.JSONObject
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+
+val headers = Headers.Builder()
+    .add("accept", "application/json, text/javascript, */*; q=0.01")
+    .add("accept-language", "es-419,es;q=0.8")
+    .add("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
+    .add("origin", RestClient.BASE_URL)
+    .add("x-requested-with", "XMLHttpRequest")
+
+
+suspend fun String.episodes(client: RestClient): List<Episode> {
+
+    /**
+     * Getting the chapter page with okhttp and parsing with jsoup
+     */
+    val document: Document = Jsoup.parse(this)
+
+    val token = document.select(CSRF_TOKEN).attr("content")
+    val internalApi = document.select(CSS_SELECTOR_INTERNAL_API).attr("data-ajax")
+
+    val referer = document.location()
+
+    headers["referer"] = referer
+    var formBody = FormBody.Builder().add(PAYLOAD_TOKEN, token).build()
+
+
+    var responseBody = client.postRequest(
+        internalApi,
+        headers.build(),
+        formBody
+    )
+
+    val jsonObject = JSONObject(responseBody)
+
+    val (total, perPage, paginateUrl) = Triple(
+        jsonObject.getJSONArray(CHAPTER_EPISODES).length(),
+        jsonObject.getInt(CHAPTER_EPISODES_PER_PAGE),
+        jsonObject.getString(CHAPTER_EPISODES_PAGINATE)
+    )
+
+    val pages = (total / perPage) + 1
+
+    val episodes = mutableListOf<Episode>()
+
+    (1..pages).forEach { page ->
+
+        formBody = FormBody.Builder()
+            .add(PAYLOAD_TOKEN, token)
+            .add(PAYLOAD_PAGE, "$page")
+            .build()
+
+        responseBody = client.postRequest(
+            paginateUrl,
+            headers.build(),
+            formBody
+        )
+
+        val arrayEpisodes = JSONObject(responseBody).getJSONArray("caps")
+
+        episodes.addAll(arrayEpisodes.toEpisodeList())
+    }
+
+    return episodes
+}

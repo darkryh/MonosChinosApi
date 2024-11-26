b810e4cdff995fda6f78190ca0dbee720548c855
darkryh <alex_torres-xc@hotmail.com>
2024-11-25 19:28:33 -0500

    updated gradle android into kotlin-java library

--- Monoschinos/build.gradle.kts
+++ Monoschinos/build.gradle.kts
@@ -1,66 +1,27 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    id("maven-publish")
+    id("java-library")
+    alias(libs.plugins.kotlin.jvm)
 }
 
-android {
-    namespace = "com.ead.lib.monoschinos"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 21
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            afterEvaluate {
-                from(components["release"])
-            }
-
-            groupId = "com.ead.lib"
-            artifactId = "monoschinos"
-            version = "0.0.8"
-        }
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
 }
 
 dependencies {
-    implementation(libs.androidx.appcompat)
     implementation(libs.webscraper)
     testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-    // define a BOM and its version
     implementation(platform(libs.okhttp.bom))
-
-    // define any required OkHttp artifacts without version
     implementation(libs.okhttp)
     implementation(libs.logging.interceptor)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.json)
 
-    //implementation("com.github.darkryh:CloudFlare-ByPass:0.0.1")
 }

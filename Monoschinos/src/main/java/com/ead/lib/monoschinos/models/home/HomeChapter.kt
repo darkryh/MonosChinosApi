43d216774eb006b25cc64eb924af10ffe6aea4ca
darkryh <alex_torres-xc@hotmail.com>
2024-11-25 19:21:58 -0500

    homeChapter data class

---
+++ Monoschinos/src/main/java/com/ead/lib/monoschinos/models/home/HomeChapter.kt
@@ -0,0 +1,13 @@
+package com.ead.lib.monoschinos.models.home
+
+import com.ead.lib.monoschinos.core.Properties
+
+data class HomeChapter(
+    val title : String,
+    val number : Int,
+    val type : String,
+    val image : String,
+    val url : String
+) {
+    val seo get() = Properties.seoChapterRegex.find(url)?.groupValues?.get(1) ?: "null"
+}
